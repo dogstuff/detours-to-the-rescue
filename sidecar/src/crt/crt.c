c303471e26750ab2d5eae9fbcf299ed75157eba0
Lentil <fox@lentil.cloud>
2026-02-13 01:36:11 +0000

    Allow user to automatically reconcile file perms on permission error

--- sidecar/src/crt/crt.c
+++ sidecar/src/crt/crt.c
@@ -2,30 +2,56 @@
 #include "log.h"
 #include "sds.h"
 
+#include <sys/stat.h>
 #include <windows.h>
 
 void *__cdecl dttr_crt_hook_open_file_callback(const char *path, char *mode) {
 	void *result = dttr_crt_open_file_with_mode(path, mode, 0x40);
 
-	if (result)
+	// Writing to a read-only file on Windows gives EBADF instead of EACCES.
+	const bool is_likely_perm_error
+		= (errno == EACCES || errno == EPERM || (errno == EBADF && mode != NULL && strchr(mode, 'w')));
+
+	if (result || errno == ENOENT || (errno == EBADF && !is_likely_perm_error))
 		return result;
 
-	log_error(
-		"Gracefully handling failed _openfile for \"%s\" (mode \"%s\"). "
-		"This is typically the result of a permissions issue.",
-		path,
-		mode
-	);
+	if (is_likely_perm_error) {
+		log_error("Permission error opening \"%s\" (mode \"%s\"): %s", path, mode, strerror(errno));
+
+		const int minimum_needed_perms = (strchr(mode, 'r') ? _S_IREAD : 0) | (strchr(mode, 'w') ? _S_IWRITE : 0);
+
+		sds prompt = sdscatprintf(
+			sdsempty(),
+			"Failed to open file \"%s\" (mode \"%s\"): %s\n\n"
+			"This is typically the result of a permissions issue, especially if you're using Wine.\n\n"
+			"Try granting permissions 0o%03o?",
+			path,
+			mode,
+			strerror(errno),
+			minimum_needed_perms
+		);
+
+		const int choice
+			= MessageBoxA(NULL, prompt, "DttR: File Permission Error Jumpscare", MB_YESNO | MB_ICONWARNING);
+		sdsfree(prompt);
+
+		if (choice == IDYES) {
+			log_info("chmod \"%s\" 0o%03o", path, minimum_needed_perms);
+			chmod(path, minimum_needed_perms);
+			return dttr_crt_hook_open_file_callback(path, mode);
+		}
+	}
+
+	log_error("Gracefully handling failed _openfile for \"%s\" (mode \"%s\"): %s", path, mode, strerror(errno));
 
 	sds msg = sdscatprintf(
 		sdsempty(),
-		"Failed to open file \"%s\" (mode \"%s\").\n\n"
-		"This file will not be written. "
-		"This is typically the result of a permissions issue, especially if you're using Wine.",
+		"Failed to open \"%s\" (mode \"%s\"). This file will not be written.\n\n%s",
 		path,
-		mode
+		mode,
+		strerror(errno)
 	);
-	MessageBoxA(NULL, msg, "DTTR: File Error", MB_OK | MB_ICONERROR);
+	MessageBoxA(NULL, msg, "DttR: File Error", MB_OK | MB_ICONERROR);
 	sdsfree(msg);
 
 	return dttr_crt_open_file_with_mode("NUL", mode, 0x40);
